a490ebd139ddf790db9e684f5ceddde710e08f6e
patilom2114 <64944432+patilom2114@users.noreply.github.com>
2020-05-07 10:01:56 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+        $link = mysqli_connect("shareddb-t.hosting.stackcp.net", "secretdi-3133316e7d", "123456om", "secretdi-3133316e7d");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
